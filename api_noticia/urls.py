5f2451db5309e2181a6a1dc8da79447d97fddf1a
Vicente Carmona <vice.carmona@duocuc.cl>
2021-07-11 19:45:11 -0400

    ADD: urls and login

--- api_noticia/urls.py
+++ api_noticia/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from api_noticia.views import noticias
+from api_noticia.views import noticias, noticia
+from rest_app.viewsLogin import login
 
 urlpatterns = [
     path('noticias', noticias, name='noticias'),
+    path('noticia/<int:pk>', mascota, name="noticia" ),
+    path('login/',login,name='login' )
+
+
 ]
