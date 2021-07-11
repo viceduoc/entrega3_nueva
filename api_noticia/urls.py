680695a3059f0b0f89d13e6c4a3a3f20858cb870
Vicente Carmona <vice.carmona@duocuc.cl>
2021-07-11 19:46:32 -0400

    MODIFY: urls

--- api_noticia/urls.py
+++ api_noticia/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from api_noticia.views import noticias, noticia
-from rest_app.viewsLogin import login
+from api_noticia.viewsLogin import login
 
 urlpatterns = [
     path('noticias', noticias, name='noticias'),
-    path('noticia/<int:pk>', mascota, name="noticia" ),
+    path('noticia/<int:pk>', noticia, name="noticia" ),
     path('login/',login,name='login' )
 
 
