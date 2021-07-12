fde9d7b430fe235bc93209b7da925f02271d2f45
Patricio Mardones <pa.mardonesg@duocuc.cl>
2021-07-11 21:48:57 -0400

    cambios en modo de consumir las noticias

--- api_noticia/urls.py
+++ api_noticia/urls.py
@@ -3,7 +3,7 @@ from api_noticia.views import noticias, noticia
 from api_noticia.viewsLogin import login
 
 urlpatterns = [
-    path('noticias', noticias, name='noticias'),
+    path('noticias', noticias, name='noticia'),
     path('noticia/<int:pk>', noticia, name="noticia" ),
     path('login/',login,name='login' )
 
