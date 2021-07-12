0e7b86919038b57eed1e8994767ce20a1e2582e1
Vicente Carmona <vice.carmona@duocuc.cl>
2021-07-11 20:25:44 -0400

    ADD: auth to settings

--- api_noticia/views.py
+++ api_noticia/views.py
@@ -5,7 +5,7 @@ from entrega3.models import Noticia
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 
 
 
@@ -16,6 +16,7 @@ from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def noticias(request):
 #     **-----------------------------**
 #       LISTANDO TODOS LAS NOTICIAS
